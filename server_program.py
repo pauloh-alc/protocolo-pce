f0d2ff9ba65012ad7842fb475b52787887fc3a97
pauloh-alc <pauloalencar.aluno@gmail.com>
2024-08-19 17:27:27 -0300

    feat(server_program.py): Adiciona configuração inicial para servidor

--- server_program.py
+++ server_program.py
@@ -1,6 +1,19 @@
 from base_connect import Base
+import socket
 
 
 class Server(Base):
-    def __init__(self, host="localhost", port=1456):
+    def __init__(self, host="localhost", port=5000):
         super().__init__(host, port)
+
+    def init(self):
+        ADDR_FAMILY_IPV4 = socket.AF_INET
+        TCP_TYPE_SOCKET = socket.SOCK_STREAM
+
+        server = socket.socket(ADDR_FAMILY_IPV4, TCP_TYPE_SOCKET)
+        server.bind((self.host, self.port))
+        server.listen()
+
+        print(
+            f"Servidor iniciado [porta: {self.port}]. Aguardando conexão com cliente ..."
+        )
