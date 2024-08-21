39ff58c99fffc87a30117a0888fbceaba7c9082d
pauloh-alc <pauloalencar.aluno@gmail.com>
2024-08-21 00:55:00 -0300

    feat(protocolo-pce/client_server/client_program.py): Prepara o programa cliente para enviar uma primeira mensagem ao servidor. Além disso, ele confirma a conexão

--- client_server/client_program.py
+++ client_server/client_program.py
@@ -1,9 +1,15 @@
+from datetime import datetime
+
 from base_connect import Base
 import socket
 
+from protocol_format.body import Body
+from protocol_format.header import Header
+from protocol_format.message import Message
+
 
 class Client(Base):
-    def __init__(self, host="localhost", port=5000, device_id="sensor01"):
+    def __init__(self, host="localhost", port=1024, device_id="sensor01"):
         super().__init__(host, port)
         self.device_id = device_id
 
@@ -14,9 +20,21 @@ class Client(Base):
         client = socket.socket(ADDR_FAMILY_IPV4, TCP_TYPE_SOCKET)
         client.connect((self.host, self.port))
 
-        client.sendall(str.encode("Hello World"))
-        data = client.recv(1024)
-        print("Mensagem ecoada:", data.decode())
+        connect_msg = Message(
+            header=Header(
+                version_protocol=1.0,
+                message_type="SENSOR_CONNECT",
+                device_id="sensor_01",
+                timestamp=datetime.now(),
+            ),
+            body=Body(value=""),
+        )
+        # Enviando msg de conexão inicial
+        client.sendall(connect_msg.to_string().encode("utf-8"))
+
+        # Recebendo msg após a conexão ser estabelacida
+        response = client.recv(1024).decode("utf-8")
+        print(f"Resposta do servidor: {response}")
 
 
 if __name__ == "__main__":
