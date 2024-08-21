c4dc3159b1b8cfff8ff247986173e08437b0b79c
pauloh-alc <pauloalencar.aluno@gmail.com>
2024-08-21 00:52:28 -0300

    feat(protocolo-pce/client_server/server_program.py): Preparando programa servidor para processar a primeira mensagem para teste
    A mensagem testada é a SENSOR_CONNECT para o sensor se conectar ao servidor, isto é, o gerenciador

--- client_server/server_program.py
+++ client_server/server_program.py
@@ -1,9 +1,15 @@
+import threading
+
 from client_server.base_connect import Base
 import socket
 
+from protocol_format.body import Body
+from protocol_format.header import Header
+from protocol_format.message import Message
+
 
 class Server(Base):
-    def __init__(self, host="localhost", port=5000):
+    def __init__(self, host="localhost", port=1024):
         super().__init__(host, port)
         self.sensors_data = {}
         self.actuators_status = {}
@@ -20,15 +26,39 @@ class Server(Base):
             f"Servidor iniciado [porta: {self.port}]. Aguardando conexão com cliente ..."
         )
 
-        conn, adds = server.accept()
-        print("Conectado em ", adds)
         while True:
-            data = conn.recv(1024)
+            conn, adds = server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, adds))
+            thread.start()
+
+    def handle_client(self, conn, adds):
+        print(f"Conectado em {adds}")
+        while True:
+            data = conn.recv(1024).decode("utf-8")
             if not data:
-                print("Fechando a conexão")
-                conn.close()
                 break
-            conn.sendall(data)
+
+            try:
+                msg = Message.from_string(data)
+                if msg.header.message_type == "SENSOR_CONNECT":
+                    print(f"Sensor {msg.header.device_id} conectado.")
+                    self.sensors_data[msg.header.device_id] = None
+                    print(self.sensors_data)
+
+                    response_msg = Message(
+                        header=Header(
+                            version_protocol=msg.header.version_protocol,
+                            message_type="SENSOR_RESPONSE",
+                            device_id=msg.header.device_id,
+                            timestamp=msg.header.timestamp,
+                        ),
+                        body=Body(value="Conexão bem-sucedida"),
+                    )
+                    conn.sendall(response_msg.to_string().encode("utf-8"))
+            except ValueError as e:
+                print(f"Erro ao processar a mensagem do cliente!")
+
+        conn.close()
 
 
 if __name__ == "__main__":
